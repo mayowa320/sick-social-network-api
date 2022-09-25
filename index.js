632a2d971eadcf5926d3c507c433cf792a22f8da
Mayowa <mayowa320@gmail.com>
2022-09-25 08:35:06 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://mayowa320:love2Golf@cluster0.bj5o7wt.mongodb.net/?retryWrites=true&w=majority"
+);
